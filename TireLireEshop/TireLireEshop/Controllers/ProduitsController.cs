88fcf872e77e3c1470a33cbd753a3ec19a4732e8
jean luc gaman <gaman2404@gmail.com>
2020-08-15 20:37:03 +0200

    Création du ProduitsController et view index

---
+++ TireLireEshop/TireLireEshop/Controllers/ProduitsController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TireLireEshop.Repository;
+
+namespace TireLireEshop.Controllers
+{
+    public class ProduitsController : Controller
+    {
+        //création d'un constructeur
+
+        IRepository<Produits> repoProduit;
+        dbtirelireshopContext ctx;
+
+        // GET: ProduitController
+
+        public ProduitsController()
+        {
+            ctx = new dbtirelireshopContext();
+            repoProduit = new Repository<Produits>(ctx);
+        }
+
+        // GET: Produit
+        public ActionResult Index()
+        {
+            return View(repoProduit.GetAll());
+        }
+
+        // GET: ProduitController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProduitController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProduitController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProduitController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProduitController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProduitController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProduitController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
